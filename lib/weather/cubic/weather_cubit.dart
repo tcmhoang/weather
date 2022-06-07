94f1c2ba5b7c0a86a4c92c1495c46d6a1ca832cf
Conrad Hoang <tcmhoang@outlook.com>
2022-06-07 07:35:21 -0400

    feat: implement weather cubic

--- lib/weather/cubic/weather_cubit.dart
+++ lib/weather/cubic/weather_cubit.dart
@@ -12,18 +12,98 @@ class WeatherCubit extends HydratedCubit<WeatherState> {
 
   final WeatherRepository _weatherRepository;
 
-  Future<Unit> fetchWeather(String city) async => throw UnimplementedError();
+  Future<Unit> fetchWeather(String city) async {
+    if (city.isEmpty) return unit;
+
+    emit(WeatherState.loading(unit: state.unit));
+
+    final weather = await _weatherRepository
+        .getWeather(city)
+        .then((maybeWeather) => maybeWeather.map(Weather.fromRepository));
+
+    weather.fold(
+      () => emit(WeatherState.failure(unit: state.unit)),
+      (weather) => emit(
+        WeatherState.success(
+          unit: state.unit,
+          weather: _convertWeatherDegreeTo(
+            weather,
+            from: TemperatureUnits.celsius,
+            to: state.unit,
+          ),
+        ),
+      ),
+    );
+
+    return unit;
+  }
 
   Weather _convertWeatherDegreeTo(
     Weather weather, {
     required TemperatureUnits from,
     TemperatureUnits to = TemperatureUnits.fahrenheit,
   }) =>
-      throw UnimplementedError();
+      from == to
+          ? weather
+          : weather.copyWith(
+              temperature: to.isFahrenheit
+                  ? Temperature(value: weather.temperature.value.toFahrenheit())
+                  : Temperature(value: weather.temperature.value.toCelsius()),
+            );
 
-  Future<Unit> refreshWeather() async => throw UnimplementedError();
+  Future<Unit> refreshWeather() async => state.maybeMap<Future<Unit>>(
+        success: (ss) async {
+          if (ss.weather == Weather.empty) return unit;
+          final weather =
+              await _weatherRepository.getWeather(ss.weather.location).then(
+                    (maybeWeather) =>
+                        maybeWeather.map(Weather.fromRepository).map(
+                              (w) => _convertWeatherDegreeTo(
+                                w,
+                                from: TemperatureUnits.celsius,
+                                to: state.unit,
+                              ),
+                            ),
+                  );
+          weather.fold(
+            () => emit(state),
+            (weather) =>
+                emit(WeatherState.success(unit: state.unit, weather: weather)),
+          );
 
-  Unit toggleUnits() => throw UnimplementedError();
+          return unit;
+        },
+        orElse: () async => Future.value(unit),
+      );
+
+  Unit toggleUnits() {
+    final nUnit = state.unit.isFahrenheit
+        ? TemperatureUnits.celsius
+        : TemperatureUnits.fahrenheit;
+
+    return state.maybeMap(
+      success: (ss) {
+        if (ss.weather == Weather.empty) return unit;
+        emit(
+          ss.copyWith(
+            unit: nUnit,
+            weather: _convertWeatherDegreeTo(
+              ss.weather,
+              from: state.unit,
+              to: nUnit,
+            ),
+          ),
+        );
+
+        return unit;
+      },
+      orElse: () {
+        emit(state.copyWith(unit: nUnit));
+
+        return unit;
+      },
+    );
+  }
 
   @override
   WeatherState fromJson(Map<String, dynamic> json) =>
